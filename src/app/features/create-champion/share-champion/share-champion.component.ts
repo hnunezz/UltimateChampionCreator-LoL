c58dcbc0308904d17fdf8b6c18491b24137e2c69
hnunezz <nunesdealmeidahenrique@gmail.com>
2023-08-08 20:17:45 -0300

    Feat: save champion created and download image

--- src/app/features/create-champion/share-champion/share-champion.component.ts
+++ src/app/features/create-champion/share-champion/share-champion.component.ts
@@ -1,6 +1,7 @@
 import { ChampionSelect } from 'src/app/shared/models/champion-select';
 import { ChampionList } from './../../../shared/models/spell-select';
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-share-champion',
@@ -8,7 +9,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./share-champion.component.scss']
 })
 export class ShareChampionComponent {
-
   @Input() public champion: ChampionList;
   @Input() public championsSelectResult: ChampionSelect;
 
@@ -18,5 +18,30 @@ export class ShareChampionComponent {
   public get disableButton(): boolean {
     return (this.championsSelectResult.spells.filter(x => x.selected == true).length === 5)
       && this.hasChampion;
+  }
+
+  constructor(private el: ElementRef) { }
+
+  public share(): void {
+    html2canvas(this.el.nativeElement.querySelector("#capture")).then(async canvas => {
+      const link = canvas.toDataURL("image/png");
+
+      this.el.nativeElement.querySelector("#display-for-canvas").appendChild(canvas)
+
+      const container = this.el.nativeElement.querySelector("#display-for-generate-print");
+      container.innerHTML = '<img src="' + link + '" />'
+
+      this.download(link)
+    }).finally(() => console.log('completed download :D'))
+  }
+
+  private async download(link: string): Promise<void> {
+    const a = document.createElement("a");
+    a.href = link;
+    a.download = `UCC_${this.champion.name.toLowerCase()}.png`;
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   }
 }
